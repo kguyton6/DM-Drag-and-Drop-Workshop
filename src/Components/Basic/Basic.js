566e310898dc4f1ac5e69bb448637698ed2c8279
kguyton6 <kimguyton@gmail.com>
2018-09-12 09:50:03 -0600

    workshop

--- src/Components/Basic/Basic.js
+++ src/Components/Basic/Basic.js
@@ -15,6 +15,7 @@ class App extends Component {
   allowDrop(e) {
     // this method will be invoked onDragOver
     // the preventDefault method of the event needs to be invoked to enable dropping
+    e.preventDefault()
   }
 
   handleDrag(e, id) {
@@ -22,11 +23,14 @@ class App extends Component {
     // this is where we can access the dataTransfer object on the event and setData to be kept track of
     // this is also where you can change the styling of the target of the event.
     // Important to remember..the ghosted dragged item is a snapshot of the original element at the time of dragstart. So if you want the original to look different from the snapshot you are dragging, you need to use a setTimeout
+    e.dataTransfer.setData('id', id)
   }
 
   handleDrop(e) {
     // this method will be invoked onDrop
     // this is where we will getData off of the dataTransfer object and use the id we set to get the dragged element by id and appendChild to the target of the event
+    let id = e.dataTransfer.getData('id')
+    e.target.appendChild(document.getElementById(id))
   }
 
   dragEnd(e) {
@@ -44,8 +48,8 @@ class App extends Component {
           className="box draggie"
           style={{ background: box.bgColor }}
           /* ------ drag and drop attributes on the dragged element ------ */
-          // draggable
-          // onDragStart={e => this.handleDrag(e, box.id)}
+          draggable
+          onDragStart={e => this.handleDrag(e, box.id)}
           // onDragEnd={this.dragEnd}
         />
       );
@@ -56,8 +60,8 @@ class App extends Component {
         <section
           className="dropzone"
           //  ------ drag and drop attributes on the dropzone -----
-          // onDrop={this.handleDrop}
-          // onDragOver={this.allowDrop}
+          onDrop={this.handleDrop}
+          onDragOver={this.allowDrop}
         >
           <p>Drop here</p>
         </section>
